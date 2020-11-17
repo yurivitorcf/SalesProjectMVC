cbcf208aa9530bfab79e3635e19c23bd08a197bf
Yuri Vitor <yurivitorcf@gmail.com>
2020-11-17 15:54:40 -0300

    Other entities and second migration

--- SalesProjectMVC/Data/SalesProjectMVCContext.cs
+++ SalesProjectMVC/Data/SalesProjectMVCContext.cs
@@ -13,6 +13,9 @@ namespace SalesProjectMVC.Models
         {
         }
 
-        public DbSet<SalesProjectMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
