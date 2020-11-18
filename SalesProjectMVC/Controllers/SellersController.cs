ce3fa321b3385c996a263b1a2b36aa1126da4117
Yuri Vitor <yurivitorcf@gmail.com>
2020-11-18 14:00:30 -0300

    Simple Create form

--- SalesProjectMVC/Controllers/SellersController.cs
+++ SalesProjectMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesProjectMVC.Services;
+using SalesProjectMVC.Models;
 
 namespace SalesProjectMVC.Controllers
 {
@@ -20,6 +21,19 @@ namespace SalesProjectMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
