21ebdfae2a07662ceb8cac0394e12a6385e39c16
Yuri Vitor <yurivitorcf@gmail.com>
2020-11-19 13:04:05 -0300

    Update seller and custom service exception

---
+++ SalesProjectMVC/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesProjectMVC.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
